2e6e1b53a91049a9bafd3cc9d770ec647501e086
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2019-05-10 11:31:18 +0800

    Update func.py

--- rpg/continous_infinity/Test/Argument/func.py
+++ rpg/continous_infinity/Test/Argument/func.py
@@ -1,29 +1,33 @@
-import matplotlib.pylab as plt
 import numpy as np
 
-plt.figure(figsize=(6,4))
-x= np.linspace(-2 * np.pi,2 * np.pi,100)
-y= np.sin(x)
-y1 = np.cos(x)
+import matplotlib.pyplot as pt
 
-plt.ylim(-3,3)#限制X轴坐标范围
-plt.xlim(-4,4)
+def f(x):
 
-plt.xlabel("X")#X轴名称
-plt.ylabel("Y")#Y轴名称
-plt.title("sinx & cosx")#图像标题
-plt.xticks((-2 * np.pi,-np.pi,0,np.pi))#设置X轴刻度
+    return 10 + x * np.log(x)
 
-plt.plot(x,y,label= "sinx")#根据函数坐标绘制折线图
-plt.plot(x,y1,label = "cosx")
+g=np.frompyfunc(f,1,1)
 
-plt.annotate('sin(np.pi)=',xy=(-np.pi,0),xytext = (-np.pi,2),fontsize=16,arrowprops= dict(facecolor = 'black',shrink=0.01))#设置注解
+'''
 
-plt.text(1,1,"Text",fontdict={'size':16,'color':'r'})
-plt.legend()
-plt.show()
---------------------- 
-作者：一只有情调的程序猿 
-来源：CSDN 
-原文：https://blog.csdn.net/weixin_42880774/article/details/83856953 
-版权声明：本文为博主原创文章，转载请附上博文链接！
+把函数赋予ufunc属性，并记为g，接收一个参数，并返回一个参数。
+
+'''
+
+a=np.arange(0.1,100,0.00001)
+
+d=g(a)
+
+d_max=np.max(d)
+
+d_min=np.min(d)
+
+pt.figure(figsize=(5*2,2*3.65))
+
+pt.xlim((0,100))
+
+pt.ylim((0,10000))
+
+pt.plot(a,d,'-',c='g',lw=2)
+
+pt.show()
