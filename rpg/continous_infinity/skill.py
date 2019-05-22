919ef080e6279278a8ad3c260752c41a7fa50fb2
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2019-05-22 15:21:07 +0800

    Update skill.py

--- rpg/continous_infinity/skill.py
+++ rpg/continous_infinity/skill.py
@@ -5,7 +5,14 @@ from definition import *
 # Skill(self, name, senders, targets, affectSender, affectTarget)
 
 # Same Skill
-#init_hit = lambda mobs: Skill('撞击', mobs, )
+def init_hit(senders, targets):
+	def affectSender(mob):
+		mob.mp -= 3
+		
+	def affectTarget(mob):
+		mob.hp -= 3
+		
+	return Skill('撞击', senders, targets, affectSender, affectTarget)
 
 # Player Skill
 
