a5dfeff0679b2105a3d7f3da104b3f76d14a251b
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2019-06-07 10:39:48 +0800

    Update game.py

--- rpg/continous_infinity/game.py
+++ rpg/continous_infinity/game.py
@@ -25,18 +25,35 @@ def play():
 	enemyList = []
 	mobList = []
 	
+	# Every Level
 	while True:
 		level += 1
 		enemyList = getEnemyList('Level/level{}.dat'.format(level))
+		# Print Level Info
 		print('===== 当前关卡： 第{}关 ====='.format(level))
 		print('怪物列表：')
 		print('ID\t名称\t等级\tHP\t武器\t头盔\t胸甲\t腿甲\t靴子')
 		for idx in range(len(enemyList)):
 			print(idx, enemyList[idx].name, enemyList[idx].lvl, enemyList[idx].hp, enemyList[idx].weapon.name, enemyList[idx].chestplate.name, enemyList[idx].leggings.name, enemyList[idx].boots.name)
+		# Judge Mob Order
 		aveLvl = reduce(lambda x, y: x.lvl + y.lvl, enemyList) // len(enemyList)
 		mobList = enemyList + [player] if aveLvl > player.lvl else [player] + enemyList
-		
-		
+		# Every-Round Initilization
+		mobIter = None
+		# Every Round
+		while True:
+			try:
+				currentMob = mobIter.__next__()
+			except:
+				mobIter = mobList.__iter__()
+				currentMob = mobIter.__next__()
+			print('这是{}的回合！'.format(currentMob.name))
+			if currentMob.type == ENEMY:
+				# Random Skill
+				currentSkill = random.choice(currentMob.skill)
+				print('{}使用了{}技能！'.format(currentMob.name, currentSkill.name))
+			else:
+				print('命令列表：')
 	
 def option():
 	pass
